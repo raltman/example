0d601947883572ffa0912469ccf3a9e1c186b08e
raltman <ruben@rootstock.io>
2016-03-10 23:33:49 -0300

    agrego start

---
+++ Start.java
@@ -0,0 +1,66 @@
+package io.rootstock;
+
+import io.rootstock.config.RskSystemProperties;
+import io.rootstock.core.RootStock;
+import io.rootstock.core.RootStockFactory;
+import org.ethereum.cli.CLIInterface;
+import io.rootstock.mine.TxBuilder;
+import org.ethereum.peg.BtcLockClient;
+import org.ethereum.peg.BtcReleaseClient;
+import org.ethereum.rpc.JsonRpcListener;
+
+import static org.ethereum.config.SystemProperties.CONFIG;
+
+/**
+ * Created by ajlopez on 3/3/2016.
+ */
+public class Start {
+    public static void main(String args[]) throws Exception {
+        CLIInterface.call(args);
+
+        if (!CONFIG.blocksLoader().equals("")) {
+            CONFIG.setSyncEnabled(false);
+            CONFIG.setDiscoveryEnabled(false);
+        }
+
+        RootStock rootstock = RootStockFactory.createRootStock();
+
+        if (!CONFIG.blocksLoader().equals(""))
+            rootstock.getBlockLoader().loadBlocks();
+
+        rootstock.getMinerServer().start();
+
+        if (RskSystemProperties.RSKCONFIG.minerEnable()) {
+            rootstock.getMinerClient().mine();
+        }
+
+        // TODO adding transaction simulation
+        if (CONFIG.simulateTxs()) {
+            new TxBuilder().simulateTxs(rootstock);
+        }
+
+
+        if (CONFIG.isFederatorEnabled()) {
+            BtcLockClient btcLockClient = (BtcLockClient) RootStockFactory.context.getBean("btcLockClient");
+            btcLockClient.setup();
+
+            final BtcReleaseClient btcReleaseClient = (BtcReleaseClient) RootStockFactory.context.getBean("btcReleaseClient");
+            new Thread(){
+                @Override
+                public void run() {
+                    try {
+                        btcReleaseClient.setup();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }.start();
+
+        }
+
+        // TODO adding rpc
+        if (CONFIG.isRpcEnabled()) {
+            new JsonRpcListener(rootstock).start();
+        }
+    }
+}
